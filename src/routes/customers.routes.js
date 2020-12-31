df73071ae8cfebee8bd7e472c7726be79ae88372
Federico Berbara <feedee96@gmail.com>
2020-12-31 02:04:46 -0300

    Functions added to routes

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -3,14 +3,14 @@ import express from 'express';
 const router = express.Router();
 
 //* Call the controller with the methods
-import { getCustomers, getCustomersById } from '../controllers/customerController'
+import { getCustomers, getCustomersById, createNewCustomer, updateCustomer, deleteOneCustomer, deleteAllCustomers } from '../controllers/customerController'
 
 //* Here I defined the methods 
 router.get('/', getCustomers); //localhost:5000/customers/
 router.get('/id/:id', getCustomersById); //localhost:5000/customers/id/1
-router.post('/newCustomer'); //localhost:5000/customers/newCustomer
-router.put('/editCustomer/:id'); //localhost:5000/customers/editCustomer/1
-router.delete('/deleteCustomer/:id'); //localhost:5000/customers/deleteCustomer/1
-router.delete('/deleteCustomers'); //localhost:5000/customers/deleteCustomers
+router.post('/addCustomer', createNewCustomer); //localhost:5000/customers/newCustomer
+router.put('/editCustomer/:id', updateCustomer); //localhost:5000/customers/editCustomer/1
+router.delete('/deleteCustomer/:id', deleteOneCustomer); //localhost:5000/customers/deleteCustomer/1
+router.delete('/deleteCustomers', deleteAllCustomers); //localhost:5000/customers/deleteCustomers
 
 export default router;
