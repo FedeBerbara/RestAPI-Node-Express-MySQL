c5217d1c86ea588922ff9e6285528af3c1eab97f
Federico Berbara <feedee96@gmail.com>
2021-01-10 15:48:06 -0300

    Update endopints

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -8,9 +8,9 @@ import { getCustomers, getCustomersById, createNewCustomer, updateCustomer, dele
 //* Here I defined the methods 
 router.get('/', getCustomers); //localhost:5000/customers/
 router.get('/id/:id', getCustomersById); //localhost:5000/customers/id/1
-router.post('/addCustomer', createNewCustomer); //localhost:5000/customers/newCustomer
-router.put('/editCustomer/:id', updateCustomer); //localhost:5000/customers/editCustomer/1
-router.delete('/deleteCustomer/:id', deleteOneCustomer); //localhost:5000/customers/deleteCustomer/1
+router.post('/add', createNewCustomer); //localhost:5000/customers/add
+router.put('/edit/:id', updateCustomer); //localhost:5000/customers/edit/1
+router.delete('/delete/:id', deleteOneCustomer); //localhost:5000/customers/delete/1
 router.delete('/deleteCustomers', deleteAllCustomers); //localhost:5000/customers/deleteCustomers
 
 export default router;
